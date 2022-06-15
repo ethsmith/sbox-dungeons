58e2b8f83f382d717b3077469a3b7a11359d055a
Jake Evans <jacob.ee92@gmail.com>
2022-06-15 15:17:21 -0600

    SImply move around

--- code/Game.cs
+++ code/Game.cs
@@ -1,4 +1,6 @@
 ﻿
+using Sandbox;
+
 namespace Dungeons;
 
 public partial class DungeonsGame : Sandbox.Game
@@ -6,7 +8,27 @@ public partial class DungeonsGame : Sandbox.Game
 
 	public DungeonsGame()
 	{
+		if ( IsServer )
+		{
+			var light = new EnvironmentLightEntity();
+			light.Rotation = Rotation.From( -45, 15, 0 );
+			light.Color = Color.Red;
+			var watermelon = new ModelEntity( "models/sbox_props/watermelon/watermelon.vmdl" );
+			watermelon.Position = Vector3.Zero;
+		}
+	}
 
+	public override void ClientJoined( Client cl )
+	{
+		base.ClientJoined( cl );
+
+		cl.Pawn = new Pawn();
+	}
+
+	[Event.Frame]
+	public void OnFrame()
+	{
+		DebugOverlay.Sphere( Vector3.Zero, 100f, Color.Green );
 	}
 
 }
--- code/Pawn.cs
+++ code/Pawn.cs
@@ -3,18 +3,33 @@ using Sandbox;
 
 namespace Dungeons;
 
-partial class Pawn : AnimatedEntity
+partial class Pawn : Entity
 {
 
-	public override void Spawn()
+	public override void Simulate( Client cl )
 	{
-		base.Spawn();
+		base.Simulate( cl );
 
-		SetModel( "models/citizen/citizen.vmdl" );
+		Rotation = Rotation.From(Input.Rotation.Angles().WithPitch( 0 ));
+		EyeRotation = Input.Rotation;
 
-		EnableDrawing = true;
-		EnableHideInFirstPerson = true;
-		EnableShadowInFirstPerson = true;
+		var movement = new Vector3( Input.Forward, Input.Left, 0 ).Normal;
+		Velocity = EyeRotation * movement;
+		Velocity *= Input.Down( InputButton.Run ) ? 1000 : 200;
+
+		MoveHelper helper = new ( Position, Velocity );
+		helper.Trace = helper.Trace.Size( 16 );
+		if ( helper.TryMove( Time.Delta ) > 0 )
+		{
+			Position = helper.Position;
+		}
+	}
+
+	public override void FrameSimulate( Client cl )
+	{
+		base.FrameSimulate( cl );
+
+		EyeRotation = Input.Rotation;
 	}
 
 }
