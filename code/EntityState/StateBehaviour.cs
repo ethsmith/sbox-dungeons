efc9020a9f24f440b86c8bc7f105bba056bee231
Jake Evans <jacob.ee92@gmail.com>
2022-06-28 15:33:37 -0600

    Simple entity state machine

---
+++ code/EntityState/StateBehaviour.cs
@@ -0,0 +1,36 @@
+﻿
+using Sandbox;
+using System;
+
+namespace Dungeons;
+
+public class StateBehaviour
+{
+
+	public Entity Owner { get; set; }
+
+	public virtual void OnEnter() { }
+	public virtual void OnExit() { }
+	public virtual void OnSimulate() { }
+
+	protected void SetState( int newstate )
+	{
+		if ( !Owner.IsValid() )
+			return;
+
+		Owner.Components.GetOrCreate<StateComponent>().SetState( newstate );
+	}
+
+	protected void SetState<TEnum>( TEnum newState )
+		where TEnum : struct, Enum
+	{
+		SetState( Convert.ToInt32( newState ) );
+	}
+
+}
+
+public class StateBehaviour<TEntity> : StateBehaviour 
+	where TEntity : Entity
+{
+	public new TEntity Owner => base.Owner as TEntity;
+}
---
+++ code/EntityState/StateComponent.cs
@@ -0,0 +1,53 @@
+﻿
+using Sandbox;
+using System;
+using System.Collections.Generic;
+
+namespace Dungeons;
+
+internal class StateComponent : EntityComponent
+{
+
+	int currentState = -int.MaxValue + 0_1_2_3_4_5_6_7_8_9;
+
+	Dictionary<int, StateBehaviour> Behaviours = new();
+
+	public void SetState<TEnum>( TEnum newState ) where TEnum : struct, Enum => SetState( Convert.ToInt32( newState ) );
+	public void SetState( int state )
+	{
+		if ( currentState == state )
+			return;
+
+		GetBehaviour( currentState )?.OnExit();
+
+		currentState = state;
+
+		GetBehaviour( currentState )?.OnEnter();
+	}
+
+	public void SetBehaviour<TEnum>( TEnum state, StateBehaviour behaviour ) where TEnum : struct, Enum => SetBehaviour( Convert.ToInt32( state ), behaviour );
+	public void SetBehaviour( int state, StateBehaviour behaviour )
+	{
+		if ( behaviour == null )
+		{
+			Behaviours.Remove( state );
+			return;
+		}
+
+		Behaviours[state] = behaviour;
+		behaviour.Owner = Entity;
+	}
+
+	public void Simulate()
+	{
+		GetBehaviour( currentState )?.OnSimulate();
+	}
+
+	StateBehaviour GetBehaviour( int state )
+	{
+		if ( !Behaviours.ContainsKey( state ) )
+			return null;
+		return Behaviours[state];
+	}
+
+}
