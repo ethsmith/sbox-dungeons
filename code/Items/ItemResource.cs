77d70aa0fdb5b6fd4e5e332b184a05c057ff8c51
Jake Evans <jacob.ee92@gmail.com>
2022-08-15 11:14:11 -0600

    Item Implicit affixes

--- code/Items/ItemResource.cs
+++ code/Items/ItemResource.cs
@@ -1,5 +1,7 @@
 ﻿
+using Dungeons.Attributes;
 using Sandbox;
+using System.Collections.Generic;
 
 namespace Dungeons.Items;
 
@@ -12,5 +14,13 @@ internal class ItemResource : GameResource
 	public string Icon { get; set; }
 	public ItemTypes ItemType { get; set; }
 	public int Durability { get; set; }
+	public List<ImplicitAffixData> Implicits { get; set; } = new();
 
+}
+
+internal struct ImplicitAffixData
+{
+	public StatTypes Stat { get; set; }
+	public float Minimum { get; set; }
+	public float Maximum { get; set; }
 }
