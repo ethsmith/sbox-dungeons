95a8f5d5940e9b30b3d31f9aa4fcdcc1c59e3652
Jake Evans <jacob.ee92@gmail.com>
2022-08-16 17:29:51 -0600

    Add minimap

---
+++ code/UI/Minimap/Minimap.cs
@@ -0,0 +1,65 @@
+﻿
+using Sandbox;
+using Sandbox.UI;
+
+namespace Dungeons.UI;
+
+internal class Minimap : Panel
+{
+
+	public override void DrawBackground( ref RenderState state )
+	{
+		base.DrawBackground( ref state );
+
+		var draw = Render.Draw2D;
+		var dungeon = DungeonEntity.Current;
+
+		foreach ( var room in dungeon.Rooms )
+		{
+			draw.Color = Color.Black.WithAlpha( .5f );
+			var rect = WorldRectToPanel( room.WorldRect );
+			draw.BoxWithBorder( rect.Contract( 3f ), 1f, Color.White.WithAlpha( .25f ) );
+
+			draw.Color = Color.White;
+			draw.FontSize = 10;
+			draw.DrawText( rect, room.Cell?.Node?.Name ?? "_", TextFlag.Center );
+		}
+
+		foreach( var r in dungeon.Routes )
+		{
+			foreach( var door in r.Doors )
+			{
+				draw.Color = Color.White.WithAlpha( .25f );
+				var rect = WorldRectToPanel( door.WorldRect );
+				draw.Box( rect.Contract( 2f ) );
+			}
+		}
+
+		var myrect = new Rect( Local.Pawn.Position - 16f, 32 );
+		myrect = WorldRectToPanel( myrect );
+
+		draw.Color = Color.Yellow;
+		draw.Circle( myrect.Center, 5f );
+	}
+
+	private Rect WorldRectToPanel( Rect rect )
+	{
+		var dungeon = DungeonEntity.Current;
+		var worldrect = dungeon.WorldRect;
+
+		// can this be better??
+
+		var sizeFraction = rect.Size / worldrect.Size;
+		var posFraction = rect.Position / worldrect.Size;
+
+		var pos = Box.Rect.Size * posFraction;
+		var size = Box.Rect.Size * sizeFraction;
+		pos = new Vector2( Box.Rect.width - pos.y - size.y, Box.Rect.height - pos.x - size.x );
+		size = new Vector2( size.y, size.x );
+
+		pos += Box.Rect.Position;
+
+		return new Rect( pos, size );
+	}
+
+}
