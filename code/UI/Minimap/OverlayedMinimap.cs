f6fcf3a7b8f501a3e0fdd4b13a86995ad2e1e263
Jake Evans <jacob.ee92@gmail.com>
2022-08-18 14:52:41 -0600

    Overlay minimap, toggled with +iv_score

---
+++ code/UI/Minimap/OverlayedMinimap.cs
@@ -0,0 +1,17 @@
+﻿
+using Sandbox;
+
+namespace Dungeons.UI;
+
+internal class OverlayedMinimap : DungeonsPanel
+{
+
+	protected override DisplayModes DisplayMode => DisplayModes.Toggle;
+	protected override InputButton ToggleButton => InputButton.Score;
+	
+	public OverlayedMinimap()
+	{
+		AddChild<Minimap>().AddClass( "overlay" );
+	}
+
+}
